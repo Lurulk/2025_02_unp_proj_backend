4a86ddb32ac87ca152abcbc9d079625804acc1c7
Lurulk <j.eduardo.alvares.s@gmail.com>
2025-09-03 22:19:45 -0300

    models created1

---
+++ Blog/src/main/java/br/edu/unipaulistana/backend/Blog/controller/UserController.java
@@ -0,0 +1,27 @@
+package br.edu.unipaulistana.backend.Blog.controller;
+
+import br.edu.unipaulistana.backend.Blog.domainmodel.User;
+import br.edu.unipaulistana.backend.Blog.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+//http://localhost:8080/users
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+//    @Autowired
+    private final UserService userService;
+
+    @GetMapping
+    public List<User> findAll(){
+        return this.userService.findAll();
+    }
+
+}
--- Blog/src/main/java/br/edu/unipaulistana/backend/Blog/domainmodel/Role.java
+++ Blog/src/main/java/br/edu/unipaulistana/backend/Blog/domainmodel/Role.java
@@ -1,4 +1,14 @@
 package br.edu.unipaulistana.backend.Blog.domainmodel;
 
+import lombok.*;
+
+import java.util.Set;
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Role {
+    private @Getter @Setter Long id;
+    private @Getter @Setter String bio;
+    private @Getter @Setter String pictureUrl;
+    private @Getter @Setter Set<User> user;
 }
