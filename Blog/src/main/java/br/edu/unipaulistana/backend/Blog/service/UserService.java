cd984b6df4a3f372c46ea866e7910c98d07f1acb
Lurulk <j.eduardo.alvares.s@gmail.com>
2025-10-29 20:42:32 -0300

    instalação de dependencias

--- Blog/src/main/java/br/edu/unipaulistana/backend/Blog/service/UserService.java
+++ Blog/src/main/java/br/edu/unipaulistana/backend/Blog/service/UserService.java
@@ -1,8 +1,12 @@
 package br.edu.unipaulistana.backend.Blog.service;
 
 import br.edu.unipaulistana.backend.Blog.domainmodel.User;
+import br.edu.unipaulistana.backend.Blog.domainmodel.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserService {
@@ -17,4 +21,7 @@ public interface UserService {
     User update(User user);
 
     User partialUpdate(User user);
+
+
+
 }
